7f949d4b06f020ae633f478d705998bdcdfa26cd
y06 <64456257+cy0612@users.noreply.github.com>
2025-01-07 23:25:13 +0800

    Clean_Temp.bat

--- Clean_Temp.bat
+++ Clean_Temp.bat
@@ -1,6 +1,6 @@
 @echo off
 color a
-:: Check for administrator privileges
+
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Administrator privileges are required. Restarting the script...
@@ -13,13 +13,11 @@ echo Cleaning files in the %temp% folder...
 echo The current user's temporary folder path is: %temp%
 echo Deleting...
 
-:: Delete all files and subfolders in the %temp% folder
 rd /s /q "%temp%"
 mkdir "%temp%"
 
 echo The %temp% folder has been cleaned!
 
-:: Clean the system temporary folder (optional)
 set systemtemp=%windir%\Temp
 echo Cleaning the system temporary folder: %systemtemp%
 rd /s /q "%systemtemp%"
